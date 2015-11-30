996c08003a7cf9255c9ab3cc57a7c26fcbbe9999
Henrique Moco <hmoco94@vt.edu>
2015-11-29 20:20:09 -0500

    Make tests have spaces not tabs

--- tests.py
+++ tests.py
@@ -2,115 +2,127 @@ import pytest
 
 from nodb import *
 
+
 @pytest.fixture
 def populate_data():
-	return {
-		'people': [
-			{'name': 'Chance',
-			'profession': 'musician',
-			'age': 22},
-			{'name': 'Donnie Trumpet',
-			'profession': 'musician',
-			'age': False},
-			{'name': 'Peter',
-			'profession': 'producer',
-			'age': False
-			}
-		],
-		'songs': [
-			{'title': 'Miracle',
-			'by': 'Donnie Trumpet'},
-			{'title': 'Home Studio', 
-			'by': 'Chance'},
-		],
-	}
+    return {
+        'people': [
+            {'name': 'Chance',
+             'profession': 'musician',
+             'age': 22},
+            {'name': 'Donnie Trumpet',
+             'profession': 'musician',
+             'age': False},
+            {'name': 'Peter',
+             'profession': 'producer',
+             'age': False
+             }
+        ],
+        'songs': [
+            {'title': 'Miracle',
+             'by': 'Donnie Trumpet'},
+            {'title': 'Home Studio',
+             'by': 'Chance'},
+        ],
+    }
+
 
 def test_filter(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	miracles = db['songs'].filter(lambda x: x['title'] == 'Miracle')
-	assert len(list(miracles)) == 1
+    db = Nodb()
+    db.load(populate_data)
+    miracles = db['songs'].filter(lambda x: x['title'] == 'Miracle')
+    assert len(list(miracles)) == 1
+
 
 def test_find_one(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	assert db['people'].find_one(Q('name', 'eq', 'Chance'))
+    db = Nodb()
+    db.load(populate_data)
+    assert db['people'].find_one(Q('name', 'eq', 'Chance'))
+
 
 def test_update(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	db['people'].update({'profession':'rapper'}, {'name':'Chance'})
-	assert db['people'].find_one(Q('name', 'eq', 'Chance'))['profession'] == 'rapper'
+    db = Nodb()
+    db.load(populate_data)
+    db['people'].update({'profession': 'rapper'}, {'name': 'Chance'})
+    assert db['people'].find_one(Q('name', 'eq', 'Chance'))['profession'] == 'rapper'
+
 
 def test_chain(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	db['people'].filter(lambda x: x['age'] == 22).update({'profession':'rapper'})
-	assert db['people'].find_one(Q('name', 'eq', 'Chance'))['profession'] == 'rapper'
+    db = Nodb()
+    db.load(populate_data)
+    db['people'].filter(lambda x: x['age'] == 22).update({'profession': 'rapper'})
+    assert db['people'].find_one(Q('name', 'eq', 'Chance'))['profession'] == 'rapper'
+
 
 def test_Q(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	chance = db['people'].filter(
-			Q('name', 'eq', 'Chance')
-		)
-	assert len(list(chance)) == 1
-	assert list(chance)[0]['name'] == 'Chance'
+    db = Nodb()
+    db.load(populate_data)
+    chance = db['people'].filter(
+        Q('name', 'eq', 'Chance')
+    )
+    assert len(list(chance)) == 1
+    assert list(chance)[0]['name'] == 'Chance'
 
 
 def test_Q_and(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	chance = db['people'].filter(
-			Q('profession', 'eq', 'musician') & Q('age', 'eq', 22)
-		)
-	assert len(list(chance)) == 1
-	assert list(chance)[0]['name'] == 'Chance'
+    db = Nodb()
+    db.load(populate_data)
+    chance = db['people'].filter(
+        Q('profession', 'eq', 'musician') & Q('age', 'eq', 22)
+    )
+    assert len(list(chance)) == 1
+    assert list(chance)[0]['name'] == 'Chance'
+
 
 def test_Q_or(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	sox = db['people'].filter(
-			Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance')
-		)
-	assert len(list(sox)) == 2
+    db = Nodb()
+    db.load(populate_data)
+    sox = db['people'].filter(
+        Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance')
+    )
+    assert len(list(sox)) == 2
+
 
 def test_Q_big(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	sox = db['people'].filter(
-			Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance') | Q('profession', 'eq', 'producer')
-		)
-	assert len(list(sox)) == 3
+    db = Nodb()
+    db.load(populate_data)
+    sox = db['people'].filter(
+        Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance') | Q('profession', 'eq', 'producer')
+    )
+    assert len(list(sox)) == 3
+
 
 def test_Q_big_and(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	sox = db['people'].filter(
-			((Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance')) & Q('profession', 'eq', 'producer'))
-		)
-	assert len(list(sox)) == 0
+    db = Nodb()
+    db.load(populate_data)
+    sox = db['people'].filter(
+        ((Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance')) & Q('profession', 'eq', 'producer'))
+    )
+    assert len(list(sox)) == 0
+
 
 def test_Q_big_and_true(populate_data):
-	db = Nodb()
-	db.load(populate_data)
-	sox = db['people'].filter(
-			Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance') & Q('profession', 'eq', 'musician')
-		)
-	assert len(list(sox)) == 2
+    db = Nodb()
+    db.load(populate_data)
+    sox = db['people'].filter(
+        Q('name', 'eq', 'Donnie Trumpet') | Q('name', 'eq', 'Chance') & Q('profession', 'eq', 'musician')
+    )
+    assert len(list(sox)) == 2
+
 
 def test_Q_and_simple():
-	qq = Q('id', 'eq', 2) | Q('id', 'eq', 1)
-	assert qq({'id': 1}) is True 
-	assert qq({'id': 2}) is True 
-	assert qq({'id': 3}) is False
+    qq = Q('id', 'eq', 2) | Q('id', 'eq', 1)
+    assert qq({'id': 1}) is True
+    assert qq({'id': 2}) is True
+    assert qq({'id': 3}) is False
+
 
 def test_Q_and_complex():
-	qq = (Q('id', 'eq', 2) | Q('id', 'eq', 1)) & Q('type', 'eq', 'foo')
-	assert qq({'id': 1, 'type': 'foo'}) is True 
-	assert qq({'id': 2, 'type': 'foo'}) is True 
-	assert qq({'id': 3, 'type': 'foo'}) is False
+    qq = (Q('id', 'eq', 2) | Q('id', 'eq', 1)) & Q('type', 'eq', 'foo')
+    assert qq({'id': 1, 'type': 'foo'}) is True
+    assert qq({'id': 2, 'type': 'foo'}) is True
+    assert qq({'id': 3, 'type': 'foo'}) is False
 
-	assert qq({'id': 1, 'type': 'bar'}) is False
+    assert qq({'id': 1, 'type': 'bar'}) is False
 
 
